dbff04fc645ea9895fc66df171627bdc11c717cc
GrazianGomes <60518412+GrazianGomes@users.noreply.github.com>
2020-04-10 17:14:34 -0300

    Add files via upload

---
+++ Estacionamento/js/script.js
@@ -0,0 +1,20 @@
+function carro() {
+    var n1 = prompt("Quantas horas você ficou com seu carro no estacionamento ?", "");
+    var n2 = 2; valor = n1 * n2;
+    alert("O valor a ser pago é: R$" + valor + ",00");
+  }
+  
+  function moto() {
+    var n1 = prompt("Quantas horas você ficou com sua moto no estacionamento ?", "");
+    var n2 = 3;  valor = n1 * n2;
+    alert("O valor a ser pago é: R$" + valor + ",00");
+  }
+  
+  function caminhao() {
+    var n1 = prompt(
+      "Quantas horas você ficou com seu caminhão no estacionamento ?",
+      ""
+    );
+    var n2 = 4; valor = n1 * n2;
+    alert("O valor a ser pago é: R$" + valor + ",00");
+  }
